fb7821b14de64df82323d005ea94a41f7cddcf8d
Skipancho <ctg1770a@gmail.com>
2021-11-15 23:49:33 +0900

    + backgrounds - borderBG

--- app/src/main/java/com/e/parayo_app/product/ProductMainUI.kt
+++ app/src/main/java/com/e/parayo_app/product/ProductMainUI.kt
@@ -1,7 +1,9 @@
 package com.e.parayo_app.product
 
 import android.view.Gravity
+import android.view.MenuItem.SHOW_AS_ACTION_ALWAYS
 import android.view.View
+import com.e.parayo_app.R
 import org.jetbrains.anko.*
 import org.jetbrains.anko.design.navigationView
 import org.jetbrains.anko.support.v4.drawerLayout
@@ -12,6 +14,12 @@ class ProductMainUI(
     override fun createView(ui: AnkoContext<ProductMainActivity>) =
         ui.drawerLayout {
             verticalLayout {
+                toolbar{
+                    title = "Parayo"
+                    menu.add("Search")
+                        .setIcon(R.drawable.ic_baseline_search_24)
+                        .setShowAsAction(SHOW_AS_ACTION_ALWAYS)
+                }.lparams(matchParent, wrapContent)
 
             }.lparams(matchParent, matchParent)
 
