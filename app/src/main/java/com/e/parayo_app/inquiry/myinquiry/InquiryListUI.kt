a787b05502bc771080bcbcd3d589b85d095fbd2d
skipancho <ctg1770a@gmail.com>
2021-12-18 17:52:32 +0900

    내 문의 메뉴 ui 뺴고

---
+++ app/src/main/java/com/e/parayo_app/inquiry/myinquiry/InquiryListUI.kt
@@ -0,0 +1,13 @@
+package com.e.parayo_app.inquiry.myinquiry
+
+import android.view.View
+import org.jetbrains.anko.AnkoComponent
+import org.jetbrains.anko.AnkoContext
+
+class InquiryListUI (
+    private val viewModel: InquiryListViewModel
+    ) : AnkoComponent<InquiryListFragment> {
+    override fun createView(ui: AnkoContext<InquiryListFragment>): View {
+        TODO("Not yet implemented")
+    }
+}
